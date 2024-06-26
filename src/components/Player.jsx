03e4790cf24fd105773ba0d6c5bcd7dbb6f52546
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-26 13:47:20 +0100

    introducng refs connection. accessing HTML elements via refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const enteredPlayerName = useRef()
   const [ playerName, setPlayerName] = useState(null)
-  const [ submitted, setSubmitted ] = useState()
-
-  function handleChage(event){
-    setSubmitted(false)
-    setPlayerName(event.target.value)
-  }
 
   function handleClick() {
-    setSubmitted(true)
+    setPlayerName(enteredPlayerName.current.value)
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : 'unknown entity'}</h2>
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChage} value={playerName} />
+        <input ref={enteredPlayerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
