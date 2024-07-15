a96e3554dca11eea6a5196cf35de3a20f51db99b
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-07-15 12:18:08 +0100

    word correction

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -13,7 +13,7 @@ const ResultModel = forwardRef(function ResultModel({ result, targetTime }, ref)
 
   return(
     <dialog ref={dialog} className="result-modal" >
-      <h2>Your {result}</h2>
+      <h2>You {result}</h2>
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped your timer with <strong>X seconds left.</strong></p>
       <form action="" method="dialog">
